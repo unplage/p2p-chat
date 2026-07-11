80044f730093009f36053b3ec6a549997a8d72a6
jiamengying <jiamengying@users.noreply.github.com>
2026-07-11 17:59:47 +0800

    chore: bump APK version to 1.1.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.p2pchat.app"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     buildTypes {
